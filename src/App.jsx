982137186ed934e44850b53451ad348b1cb47e9b
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-11-05 20:59:09 +0000

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 
-import { useState } from 'react'
 import './App.css'
 import UserState from './components/UserState'
 
@@ -7,21 +6,10 @@ function App() {
   
   return (
     <>
-    <Furit />
-      <UserState />
-      <div> count this valur {sum ()}</div>
+    <UserState />
     </>
   )
 }
-function Furit (){
-  return(
-    <h1>Assalam u Alaikum</h1>
-  )
-}
-function sum(){
-  return(
-    10+10
-  )
-}
+
 
 export default App
--- src/components/UserState.jsx
+++ src/components/UserState.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const UserState = () => {
-    const [count,setCount]=useState(0)
+  const [count, setCount] = useState (0);
+
+  const handelCount = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
-    <div className='bg-white w-60 h-65 shadow justify-center items-center flex flex-col mt-0 rounded-2xl'> 
-    <div className='border-2 border-blue-400 w-55 h-45 rounded-2xl font-bold'>
-      {count}
+    <div className="bg-white w-60 h-65 shadow justify-center items-center flex flex-col mt-0 rounded-2xl">
+      <div className="border-2 border-blue-400 w-55 h-45 rounded-2xl font-bold">
+        {count}
+      </div>
+      {/* Event Handelers  */}
+      <button
+        onClick={handelCount}
+        className="bg-blue-500 w-50 h-10 rounded-2xl text-white font-bold mt-5"
+      >
+        Increase Value
+      </button>
+      <p>{count}</p>
+      <input onChange={(e)=>{e.target.valueAsNumber}} type="number" value={count} className="border"/>
     </div>
+  );
+};
 
-      <button className='bg-blue-500 w-50 h-10 rounded-2xl text-white font-bold mt-5'  onClick={()=>setCount(count+1)}>Increase Value</button>
-    
-    </div>
-  )
-}
-
-export default UserState
+export default UserState;
